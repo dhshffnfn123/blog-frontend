f2164f52b7e12a861b06ee114d86cefce9d87377
B H An <anbyunghoon0107@gamil.com>
2021-12-03 23:18:49 +0900

    snippet

---
+++ src/components/auth/AuthForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+// 회원가입 또는 로그인 폼을 보여준다.
+
+const AuthFormBlock = styled.div``;
+const AuthForm = () => {
+    return (
+        <AuthFormBlock>
+            AuthForm
+        </AuthFormBlock>
+    );
+};
+
+export default AuthForm;
---
+++ src/components/auth/AuthTemplate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+// 회원가입/로그인 페이지읠 레이아웃을 담당하는 컴포넌트
+
+const AuthTempleBlock = styled.div``;
+
+const AuthTemplate = () => {
+    return (
+        <AuthTempleBlock>
+
+        </AuthTempleBlock>
+    );
+};
+
+export default AuthTemplate;
