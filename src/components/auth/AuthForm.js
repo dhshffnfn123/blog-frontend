fa24c821bea5f0aacb0486ba6d3e2d2a38fcb0e8
B H An <anbyunghoon0107@gamil.com>
2021-12-04 23:52:02 +0900

    type props에 따라 다른 내용 보여주기

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -44,14 +44,20 @@ const Footer = styled.div`
     }
 `;
 
-const ButtomWithMarginTop = styled(Button)`
+const ButtonWithMarginTop = styled(Button)`
     margin-top: 1rem;
 `;
 
-const AuthForm = () => {
+const textMap = {
+    login: "로그인",
+    register: "회원가입",
+};
+
+const AuthForm = ({ type }) => {
+    const text = textMap[type];
     return (
         <AuthFormBlock>
-            <h3>로그인</h3>
+            <h3>{text}</h3>
             <form>
                 <StyledInput autoComplete="username" name="username" placeholder="아이디" />
                 <StyledInput
@@ -60,10 +66,24 @@ const AuthForm = () => {
                     placeholder="비밀번호"
                     type="password"
                 />
-                <Button cyan fullWidth style={{ marginTop: '1rem' }}>로그인</Button>  {/* cyan={true} fullWidth={true}와 같다. */}
+                {type === 'register' && (
+                    <StyledInput
+                        autoComplete="new-password"
+                        name="passwordConfirm"
+                        placeholder="비밀번호 확인"
+                        type="password"
+                    />
+                )}
+                <ButtonWithMarginTop cyan fullWidth> {/* cyan={true} fullWidth={true}와 같다. */}
+                    {text}
+                </ButtonWithMarginTop> 
                 </form>
                 <Footer>
-                    <Link to="/register">회원가입</Link>
+                    {type === 'login' ? (
+                        <Link to="/register">회원가입</Link>
+                    ) : (
+                        <Link to="/login">로그인</Link>
+                    )}
                 </Footer>
         </AuthFormBlock>
     );
