079ff2505104ce2b2e4cc0b5c5a3512760044d74
B H An <anbyunghoon0107@gamil.com>
2021-12-04 23:34:05 +0900

    기본적인 UI 구현 중 / Button props로 스타일 적용

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -1,13 +1,70 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import palette from "../../lib/styles/palette";
+import Button from "../common/Button";
 
 // 회원가입 또는 로그인 폼을 보여준다.
 
-const AuthFormBlock = styled.div``;
+const AuthFormBlock = styled.div`
+    h3 {
+        margin: 0;
+        color: ${palette.gray[8]};
+        margin-bottom: 1rem;
+    }
+`;
+
+// 스타일링 된 input
+const StyledInput = styled.input`
+    font-size: 1rem;
+    border: none;
+    border-bottom: 1px solid ${palette.gray[5]};
+    padding-bottom: 0.5rem;
+    outline: none;
+    width: 100%;
+    &:focus {
+        color: $oc-teal-7;
+        border-bottom: 1px solid ${palette.gray[7]};
+    }
+    & + & {
+        margin-top: 1rem;
+    }
+`;
+
+// 폼 하단에 로그인 혹은 회원가입 링크를 보여 줌
+const Footer = styled.div`
+    margin-top: 2rem;
+    text-align: right;
+    a {
+        color: ${palette.gray[6]};
+        text-decoration: underline;
+        &:hover {
+            color: ${palette.gray[9]};
+        }
+    }
+`;
+
+const ButtomWithMarginTop = styled(Button)`
+    margin-top: 1rem;
+`;
+
 const AuthForm = () => {
     return (
         <AuthFormBlock>
-            AuthForm
+            <h3>로그인</h3>
+            <form>
+                <StyledInput autoComplete="username" name="username" placeholder="아이디" />
+                <StyledInput
+                    autoComplete="new-password"
+                    name="password"
+                    placeholder="비밀번호"
+                    type="password"
+                />
+                <Button cyan fullWidth style={{ marginTop: '1rem' }}>로그인</Button>  {/* cyan={true} fullWidth={true}와 같다. */}
+                </form>
+                <Footer>
+                    <Link to="/register">회원가입</Link>
+                </Footer>
         </AuthFormBlock>
     );
 };
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import palette from '../../lib/styles/palette';
 
 const StyledButton = styled.button`
@@ -16,6 +16,25 @@ const StyledButton = styled.button`
     &:hover {
         background: ${palette.gray[6]};
     }
+    ${props => 
+        props.fullWidth &&
+        css`
+            padding-top: 0.75rem;
+            padding-bottom: 0.75rem;
+            width: 100%;
+            font-size: 1.125rem;
+        `}
+
+    ${props => 
+        props.cyan &&
+        css`
+            background: ${palette.cyan[5]};
+            &:hover {
+                background: ${palette.cyan[4]};
+            }
+        `}
+    }
+    
     `;
 
     const Button = props => <StyledButton { ...props} />; // 버튼이 받아오는 props를 모두 StyledButton에 전달함
