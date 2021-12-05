e4ca3e24e178b69b3fdf43cb6dd7b09d5d438a07
B H An <anbyunghoon0107@gamil.com>
2021-12-05 23:50:51 +0900

    오류 수정

--- src/modules/loading.js
+++ src/modules/loading.js
@@ -1,5 +1,4 @@
 import { createAction, handleActions } from 'redux-actions';
-import { action } from '../../node_modules/commander/typings/index';
 
 const START_LOADING = 'loading/START_LOADING';
 const FINISH_LOADING = 'loading/FINISH_LOADING';
