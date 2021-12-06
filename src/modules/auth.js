3fc94c4a0c05ab806ad5f484f0ebffce90fdb66f
B H An <anbyunghoon0107@gamil.com>
2021-12-06 14:36:16 +0900

    auth.js 간소화 전

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,8 +1,17 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from 'immer';
+import { createRequestActionTypes } from '../lib/createRequestSaga';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const REGISTER = 'auth/REGISTER';
+const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+
+const LOGIN = 'auth/LOGIN';
+const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
 
 export const changeField = createAction(
     CHANGE_FIELD,
