00dd45ead06bec14e59489ea8ce6383837fb6543
B H An <anbyunghoon0107@gamil.com>
2021-12-06 15:08:02 +0900

    useEffect 적용

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from "../../components/auth/AuthForm";
 
 const RegisterForm = () => {
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
-        form: auth.register
+    const { form, auth, authError } = useSelector(({ auth }) => ({
+        form: auth.register,
+        auth: auth.auth,
+        authError: auth.authError
     }));
     // 인풋 변경 이벤트 핸들러
     const onChange = e => {
@@ -23,13 +25,31 @@ const RegisterForm = () => {
     // 폼 등록 이벤트 핸들러
     const onSubmit = e => {
         e.preventDefault();
-        // 구현 예정
+        const { username, password, passwordConfirm } = form;
+        if (password !== passwordConfirm) {
+            // TODO: 오류 처리
+            return;
+        }
+        dispatch(register({ username, password }));
     };
 
     // 컴포넌트가 처음 렌더링될 때 form을 초기화함
     useEffect(() => {
         dispatch(initializeForm('register'));
     }, [dispatch]);
+
+    // 회원 가입 성공 / 실패 처리
+    useEffect(() => {
+        if (authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            return;
+        }
+        if (auth) {
+            console.log('회원가입 성공');
+            console.log(auth);
+        }
+    }, [auth, authError])
 
     return (
         <AuthForm
