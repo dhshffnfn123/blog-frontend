0587ea2dac36c976e309a5e1c3b5223931d49422
B H An <anbyunghoon0107@gamil.com>
2021-12-07 09:48:21 +0900

    check API 적용

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -2,13 +2,15 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from "../../components/auth/AuthForm";
+import user, { check } from '../../modules/user';
 
 const RegisterForm = () => {
     const dispatch = useDispatch();
-    const { form, auth, authError } = useSelector(({ auth }) => ({
+    const { form, auth, authError } = useSelector(({ auth, user }) => ({
         form: auth.register,
         auth: auth.auth,
-        authError: auth.authError
+        authError: auth.authError,
+        user: user.user
     }));
     // 인풋 변경 이벤트 핸들러
     const onChange = e => {
@@ -48,8 +50,17 @@ const RegisterForm = () => {
         if (auth) {
             console.log('회원가입 성공');
             console.log(auth);
+            dispatch(check());
         }
-    }, [auth, authError]);
+    }, [auth, authError, dispatch]);
+
+    // user 값이 잘 설정 되었는지 확인
+    useEffect(() => {
+        if (user) {
+            console.log('check API 성공');
+            console.log(user);
+        }
+    }, [user]);
 
     return (
         <AuthForm
