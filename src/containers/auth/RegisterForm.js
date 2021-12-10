bdd4d2f04b81ac0331521a3dfd41dce51caef455
B H An <anbyunghoon0107@gamil.com>
2021-12-10 14:34:28 +0900

    회원가입 및 로그인 오류 처리

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from "../../components/auth/AuthForm";
 import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
     const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
         form: auth.register,
@@ -28,8 +30,16 @@ const RegisterForm = () => {
     const onSubmit = e => {
         e.preventDefault();
         const { username, password, passwordConfirm } = form;
+        // 하나라도 비어 있다면
+        if ([username, password, passwordConfirm].includes('')) {
+            setError('빈 칸을 모두 입력하세요');
+            return;
+        }
+        // 비밀번호가 일치하지 않는다면
         if (password !== passwordConfirm) {
-            // TODO: 오류 처리
+            setError('비밀번호가 일치하지 않습니다.');
+            dispatch(changeField({ form: 'register', key: 'password', value: '' }));
+            dispatch(changeField({ form: 'register', key: 'passwordConfirm', value: '' }));
             return;
         }
         dispatch(register({ username, password }));
@@ -43,8 +53,13 @@ const RegisterForm = () => {
     // 회원 가입 성공 / 실패 처리
     useEffect(() => {
         if (authError) {
-            console.log('오류 발생');
-            console.log(authError);
+            // 계정명이 이미 존재할 때
+            if (authError.response.status === 409) {
+                setError('이미 존재하는 계정입니다.');
+                return;
+            }
+            // 기타 이유
+            console.log('회원가입 실패');
             return;
         }
         if (auth) {
@@ -64,14 +79,22 @@ const RegisterForm = () => {
         }
     }, [user]);
 
+    // user 값이 잘 설정되었는지 확인
+    useEffect(() => {
+        if (user) {
+            history.push('/'); // 홈 화면으로 이동
+        }
+    },[history, user]);
+
     return (
         <AuthForm
             type="register"
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
