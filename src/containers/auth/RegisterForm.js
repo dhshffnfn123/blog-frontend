8893308524c22ab1da0d96ae497e61e4112cc3af
B H An <anbyunghoon0107@gamil.com>
2021-12-10 16:03:51 +0900

    localstorage에 사용자 정보 저장

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -83,6 +83,11 @@ const RegisterForm = ({ history }) => {
     useEffect(() => {
         if (user) {
             history.push('/'); // 홈 화면으로 이동
+            try {
+                localStorage.setItem('user', JSON.stringify(user));
+            } catch (e) {
+                console.log('localStorage is not working');
+            }
         }
     },[history, user]);
 
