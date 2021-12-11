11d7a8c4e0e8ae39c74b7c6a026ca5c154f3118a
B H An <anbyunghoon0107@gamil.com>
2021-12-11 22:01:18 +0900

    Post UI 준비

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import HeaderContainer from "../containers/common/HeaderContainer";
+import PostViewer from "../components/post/PostViewer";
 
 const PostPage = () => {
-    return <div>읽기</div>;
+    return (
+        <>
+            <HeaderContainer />
+            <PostViewer />
+        </>
+    );
 };
 
 export default PostPage;
