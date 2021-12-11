04d02646c036798159505db438edc5b73c66f2f8
B H An <anbyunghoon0107@gamil.com>
2021-12-11 22:49:47 +0900

    포스트 작성 구현

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import HeaderContainer from "../containers/common/HeaderContainer";
-import PostViewer from "../components/post/PostViewer";
+import PostViewerContainer from "../containers/post/PostViewerContainer";
 
 const PostPage = () => {
     return (
         <>
             <HeaderContainer />
-            <PostViewer />
+            <PostViewerContainer />
         </>
     );
 };
