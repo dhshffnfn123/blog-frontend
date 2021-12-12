a2b0fec13458b53bb8405d7755c7c8171c037efa
B H An <anbyunghoon0107@gamil.com>
2021-12-12 14:50:45 +0900

    Post 목록 조회 API 연동

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,8 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
 import Responsive from "../common/Responsive";
 import Button from "../common/Button";
 import palette from "../../lib/styles/palette";
+import SubInfo from "../common/SubInfo";
+import Tags from "../commons/Tags";
 
 const PostListBlock = styled(Responsive)`
     margin-top: 3rem;
@@ -38,46 +41,12 @@ const PostItemBlock = styled.div`
     }
 `;
 
-const SubInfo = styled.div`
-    /* margin-top: 1rem; */
-    color: ${palette.gray[6]};
-
-    /* span 사이에 가운뎃점 문자 보여주기 */
-    span + span:before {
-        color: ${palette.gray[4]};
-        padding-left: 0.25rem;
-        padding-right: 0.25rem;
-        content: '\\B7'; /* 가운뎃점 문자 */
-    }
-`;
-
-const Tags = styled.div`
-    margin-top: 0.5rem;
-    .tag {
-        display: inline-block;
-        color: ${palette.cyan[7]};
-        text-decoration: none;
-        margin-right: 0.5rem;
-        &:hover {
-            color: ${palette.cyan[6]};
-        }
-    }
-`;
-
 const PostItem = () => {
     return (
         <PostItemBlock>
             <h2>제목</h2>
-            <SubInfo>
-                <span>
-                    <b>username</b>
-                </span>
-                <span>{new Date().toLocaleDateString()}</span>
-            </SubInfo>
-            <Tags>
-                <div className="tag">#태그1</div>
-                <div className="tag">#태그2</div>
-            </Tags>
+            <SubInfo username="username" publishedDate={new Date()} />
+            <Tags tags={['태그1', '태그2', '태그3']} />
             <p>포스트 내용의 일부분..</p>
         </PostItemBlock>
     );
