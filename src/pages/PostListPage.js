c845c8316d924b4d50eb0cdc93a8decf5059ae91
B H An <anbyunghoon0107@gamil.com>
2021-12-12 14:51:10 +0900

    사용자 로그인 중에만 작성 버튼 렌더링

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import HeaderContainer from "../containers/common/HeaderContainer";
+import PostListContainer from "../containers/posts/PostListContainer";
 
 const PostListPage = () => {
     return (
     <>
         <HeaderContainer />
-        <div>안녕하세요.</div>
+        <PostListContainer />
     </>
     );
 };
