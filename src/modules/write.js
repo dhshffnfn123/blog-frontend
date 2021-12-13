c71e241db1cb3e99acece4ce50227452c159d744
B H An <anbyunghoon0107@gamil.com>
2021-12-13 14:44:30 +0900

    Post가 자신의 포스트 일때만 수정 삭제가 나타나게 적용

--- src/modules/write.js
+++ src/modules/write.js
@@ -12,6 +12,7 @@ const [
     WRITE_POST_SUCCESS,
     WRITE_POST_FAILURE,
 ] = createRequestActionTypes('write/WRITE_POST'); // 포스트 작성
+const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST';
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
@@ -23,6 +24,7 @@ export const writePost = createAction(WRITE_POST, ({ title, body, tags }) => ({
     body,
     tags,
 }));
+export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post);
 
 // 사가 생성
 const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
@@ -36,6 +38,7 @@ const initialState = {
     tags: [],
     post: null,
     postError: null,
+    originalPostId: null,
 };
 
 const write = handleActions(
@@ -60,6 +63,13 @@ const write = handleActions(
         [WRITE_POST_FAILURE]: (state, { payload: postError }) => ({
             ...state,
             postError,
+        }),
+        [SET_ORIGINAL_POST]: (state, { payload: post }) => ({
+            ...state,
+            title: post.title,
+            body: post.body,
+            tags: post.tags,
+            originalPostId: post._id,
         }),
     },
     initialState,
