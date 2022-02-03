e9407a8ef3bc802b5e8b8e54c4a98218a7bcf25c
B H An <anbyunghoon0107@gamil.com>
2022-02-03 17:09:38 +0900

    들여쓰기 변경

--- src/modules/user.js
+++ src/modules/user.js
@@ -2,16 +2,14 @@ import { createAction, handleActions } from 'redux-actions';
 import { takeLatest, call } from 'redux-saga/effects';
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {
-    createRequestActionTypes,
+  createRequestActionTypes,
 } from '../lib/createRequestSaga';
 
 const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
 // 회원 정보 확인
-const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
-    'user/CHECK',
-);
+const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
+  createRequestActionTypes('user/CHECK');
 const LOGOUT = 'user/LOGOUT';
-
 
 export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
 export const check = createAction(CHECK);
@@ -20,54 +18,53 @@ export const logout = createAction(LOGOUT);
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
 
 function checkFailureSaga() {
-    try {
-        localStorage.removeItem('user'); // localStorage에서 user를 제거
-    } catch (e) {
-        console.log('localStorage is not working');
-    }
+  try {
+    localStorage.removeItem('user'); // localStorage에서 user를 제거
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
 }
 
 function* logoutSaga() {
-    try {
-        yield call(authAPI.logout); // logout API 호출
-        localStorage.removeItem('user'); // localStorage에서 user를 제거
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    yield call(authAPI.logout); // logout API 호출
+    localStorage.removeItem('user'); // localStorage에서 user를 제거
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-
 export function* userSaga() {
-    yield takeLatest(CHECK, checkSaga);
-    yield takeLatest(CHECK_FAILURE, checkFailureSaga);
-    yield takeLatest(LOGOUT, logoutSaga);
+  yield takeLatest(CHECK, checkSaga);
+  yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
-    user: null,
-    checkError: null,
+  user: null,
+  checkError: null,
 };
 
 export default handleActions(
-    {
-        [TEMP_SET_USER]: (state, { payload: user }) => ({
-            ...state,
-            user,
-        }),
-        [CHECK_SUCCESS]: (state, { payload: user }) => ({
-            ...state,
-            user,
-            checkError: null,
-        }),
-        [CHECK_FAILURE]: (state, { payload: error }) => ({
-            ...state,
-            user: null,
-            checkError: error,
-        }),
-        [LOGOUT]: state => ({
-            ...state,
-            user: null,
-        }),
-    },
-    initialState,
+  {
+    [TEMP_SET_USER]: (state, { payload: user }) => ({
+      ...state,
+      user,
+    }),
+    [CHECK_SUCCESS]: (state, { payload: user }) => ({
+      ...state,
+      user,
+      checkError: null,
+    }),
+    [CHECK_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      user: null,
+      checkError: error,
+    }),
+    [LOGOUT]: (state) => ({
+      ...state,
+      user: null,
+    }),
+  },
+  initialState,
 );
