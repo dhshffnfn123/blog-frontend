11d2805ba04a52f493ebc046b2466280d45f5dfb
B H An <anbyunghoon0107@gamil.com>
2022-02-03 23:00:05 +0900

    prettier

--- src/components/post/PostViewer.js
+++ src/components/post/PostViewer.js
@@ -1,60 +1,62 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
-import palette from "../../lib/styles/palette";
-import Responsive from "../common/Responsive";
-import SubInfo from "../common/SubInfo";
-import Tags from "../common/Tags";
-import { Helmet } from "react-helmet-async";
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+import SubInfo from '../common/SubInfo';
+import Tags from '../common/Tags';
+import { Helmet } from 'react-helmet-async';
 
 const PostViewerBlock = styled(Responsive)`
-    margin-top: 4rem;
+  margin-top: 4rem;
 `;
 const PostHead = styled.div`
-    border-bottom: 1px solid ${palette.gray[2]};
-    padding-bottom: 3rem;
-    margin-bottom: 3rem;
-    h1 {
-        font-size: 3rem;
-        line-height: 1.5;
-        margin: 0;
-    }
+  border-bottom: 1px solid ${palette.gray[2]};
+  padding-bottom: 3rem;
+  margin-bottom: 3rem;
+  h1 {
+    font-size: 3rem;
+    line-height: 1.5;
+    margin: 0;
+  }
 `;
 
 const PostContent = styled.div`
-    font-size: 1.3125rem;
-    color: ${palette.gray[8]};
+  font-size: 1.3125rem;
+  color: ${palette.gray[8]};
 `;
 
 const PostViewer = ({ post, error, loading, actionButtons }) => {
-    // 에러 발생 시
-    if (error) {
-        if (error.response && error.response.status === 404) {
-            return <PostViewerBlock>존재하지 않는 포스트입니다.</PostViewerBlock>;
-        }
-        return <PostViewerBlock>오류 발생!</PostViewerBlock>;
+  // 에러 발생 시
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <PostViewerBlock>존재하지 않는 포스트입니다.</PostViewerBlock>;
     }
+    return <PostViewerBlock>오류 발생!</PostViewerBlock>;
+  }
 
-    // 로딩 중이거나 아직 포스트 데이터가 없을 때
-    if (loading || !post) {
-        return null;
-    }
-    const { title, body, user, publishedDate, tags } = post;
-    return (
-        <PostViewerBlock>
-            <Helmet>
-                <title>{title} - REACTERS</title>
-            </Helmet>
-            <PostHead>
-                <h1>{title}</h1>
-                <SubInfo username={user.username} publishedDate={publishedDate} hasMarginTop />
-                <Tags tags={tags} />
-            </PostHead>
-            {actionButtons} {/* props를 JSX 형태로 받아와서 렌더링하는 것이다. */}
-            <PostContent
-                dangerouslySetInnerHTML={{__html: body }}
-            />
-        </PostViewerBlock>
-    );
+  // 로딩 중이거나 아직 포스트 데이터가 없을 때
+  if (loading || !post) {
+    return null;
+  }
+  const { title, body, user, publishedDate, tags } = post;
+  return (
+    <PostViewerBlock>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <PostHead>
+        <h1>{title}</h1>
+        <SubInfo
+          username={user.username}
+          publishedDate={publishedDate}
+          hasMarginTop
+        />
+        <Tags tags={tags} />
+      </PostHead>
+      {actionButtons} {/* props를 JSX 형태로 받아와서 렌더링하는 것이다. */}
+      <PostContent dangerouslySetInnerHTML={{ __html: body }} />
+    </PostViewerBlock>
+  );
 };
 
 export default PostViewer;
